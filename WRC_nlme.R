a3c148e19b9d7ec3acf7b0a614704fbb3aca0127
mgwalsh <markusgwalsh@gmail.com>
2018-12-03 10:26:09 +0300

    2-compartment soil water-retention curve model with random soil/site effects

--- WRC_nlme.R
+++ WRC_nlme.R
@@ -1,5 +1,5 @@
 # 2-compartment soil water-retention curve model with random soil/site effects
-# note that this is a starter setup using partial data from central Kenya, using the <nlmer> package
+# note that this is a starter setup using partial data from central Kenya, using the <nlme> package
 # M. Walsh, November 2018
 
 # Required packages
