080ee63b950aef3d9b9208dfcb16ee5eb1c6ab3a
mgwalsh <markusgwalsh@gmail.com>
2018-12-04 18:10:49 +0300

    2-compartment soil water-retention curve model with random soil/site effects

--- WRC_nlme.R
+++ WRC_nlme.R
@@ -26,7 +26,7 @@ summary(vwc.nls)
 plot(vwc.nls, resid(.) ~ fitted(.) | sid, abline = 0, grid=F)
 
 # No pooling model <nlsList> ----------------------------------------------
-gwrc <- groupedData(vwc ~ pf | sid, as.data.frame(wrc))
+gwrc <- groupedData(vwc ~ pf | sid, as.data.frame(wrc)) ## group measurements by sites
 vwc.lis <- nlsList(vwc~SSbiexp(pf,s1,r1,s2,r2), gwrc) 
 plot(augPred(vwc.lis, level=0:1), xlab ="pF (bars)", ylab = "Volumetric water content (v/v)") ## plot of site/sid level fits
 plot(intervals(vwc.lis))
@@ -45,4 +45,5 @@ par(pty="s", mfrow = c(1,2))
 plot(gwrc$vwc ~ fitted(vwc.nlme), xlim = c(0,0.8), ylim = c(0,0.8), xlab = "Fitted", ylab = "Measured")
 abline(c(0,1))
 plot(resid(vwc.nlme) ~ fitted(vwc.nlme), xlim = c(0,0.8), xlab="Fitted", ylab = "Residual") ## residual plot
+abline(h=0)
 
